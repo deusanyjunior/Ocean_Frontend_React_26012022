edc4f82fa48cb6b8c731ceefa0be63b930679526
dj <dj@ime.usp.br>
2022-01-27 15:05:13 -0300

    Adicionando modulo react-calendar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Calendario from "./components/Calendario/Calendario";
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 ReactDOM.render(
-  <Calendario />
+  <div>
+    <Calendario />  
+    <Calendar />
+  </div>
   ,
   document.getElementById('root')
 )
